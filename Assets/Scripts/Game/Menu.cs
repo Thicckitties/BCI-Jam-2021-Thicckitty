31cf219e78ce79e4d04d7807cfafc455ba466188
DominickTamras <31298279+DominickTamras@users.noreply.github.com>
2021-10-24 22:17:28 -0700

    ugh

---
+++ Assets/Scripts/Game/Menu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace Thicckitty
+{
+    public class Menu : MonoBehaviour
+    {
+        public void EnterGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+
+    }
+}
