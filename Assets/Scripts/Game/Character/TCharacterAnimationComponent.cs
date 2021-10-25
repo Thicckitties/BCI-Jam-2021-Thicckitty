03467d05946d3a87c6349d72c156b3c4ee856d6b
ffsjkorn <jkorn2324swagg@gmail.com>
2021-10-24 23:27:56 -0700

    Kick animation in

---
+++ Assets/Scripts/Game/Character/TCharacterAnimationComponent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Thicckitty
+{
+    [RequireComponent(typeof(Animator))]
+    public class TCharacterAnimationComponent : MonoBehaviour
+    {
+        public delegate void KickAnimationFinishedEventDelegate();
+        public KickAnimationFinishedEventDelegate FinishedKickEvent;
+
+        private Animator _animator;
+
+        public Animator Animator
+        {
+            get
+            {
+                _animator ??= GetComponent<Animator>();
+                return _animator;
+            }
+        }
+        
+        public void KickAnimationFinished()
+        {
+            FinishedKickEvent?.Invoke();            
+        }
+    }
+}
